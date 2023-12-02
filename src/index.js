a658eba20d33a0e530c71628bd8cbdbffd7a9b06
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-12-02 17:17:58 +0100

    deploy to firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 const app = express();
 // const bodyParser = require("body-parser");
 const router = require("./routes");
 
-const port = process.env.PORT || 5000;
+const port = process.env.OUT_PORT || 5000;
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 //Parse incoming requests with JSON payloads
 // app.use(bodyParser.json());
@@ -28,4 +28,4 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
