3133f2767db9667f5945a1c3d9889e4e2ba85459
ucheBest <akah.uchechi@gmail.com>
2024-04-24 00:02:23 +0100

    fix server

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const router = require("./routes");
 
 const port = process.env.OUT_PORT || 5000;
-// const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
 //Parse incoming requests with JSON payloads
 // app.use(bodyParser.json());
@@ -16,19 +16,9 @@ const port = process.env.OUT_PORT || 5000;
 //Use the "cors" middleware to enable CORS
 app.use(
   cors({
-    origin: [
-      // FRONTEND_URL,
-      "https://us-central1-mfoh-server.cloudfunctions.net",
-      "http://localhost:5000",
-      "http://localhost:5173",
-    ],
+    origin: [FRONTEND_URL],
   })
 );
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
 
 // Middleware for parsing JSON requests
 app.use(express.json());
